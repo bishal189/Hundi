063758832b8a5972d89dc9709ac69b98530e6a3d
bishal189 <bishalmurmu150@gmail.com>
2023-12-12 07:47:53 +0545

    update

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://bishal111.pythonanywhere.com/', 
+  baseURL: 'http://127.0.0.1:8000', 
   withCredentials: true,  // Include credentials (cookies) in the request
   // Make sure this is correct
   // other Axios configuration options
