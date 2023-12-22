b87ca9c111e8edda1e42d32c10274174dc014c3f
bishal189 <bishalmurmu150@gmail.com>
2023-12-22 22:50:22 +0545

    completed wallet and request

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
- baseURL:'http://127.0.0.1:8000/',
-  //baseURL: 'https://bishal111.pythonanywhere.com/', 
+ //baseURL:'http://127.0.0.1:8000/',
+  baseURL: 'https://bishal111.pythonanywhere.com/', 
   withCredentials: true,  // Include credentials (cookies) in the request
   // Make sure this is correct
   // other Axios configuration options
